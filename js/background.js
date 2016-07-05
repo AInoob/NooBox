70cbedca3fa94d94365ff58afcd53685f3a42424
ainoob <ainoob@qq.com>
2016-07-05 06:36:14 +0000

    fix image search bugs

--- js/background.js
+++ js/background.js
@@ -43,29 +43,41 @@ NooBox.Image.fetchFunctions.google=function(cursor,data){
   page=$(data);
   var keyword=page.find('._gUb').text();
   var relatedWebsites=[];
-  var relatedWebsiteLinks=page.find('.srg').first().find('.g').find('.r').find('a');
-  var relatedWebsiteDescriptions=page.find('.srg').first().find('.g').find('.s').find('.st');
-  for(var i=0;i<relatedWebsiteLinks.length;i++){
+  var relatedWebsiteList=$(page.find('#rso').find('.rgsep')[0]).prev().find('.rc')
+  for(var i=0;i<relatedWebsiteList.length;i++){
     var website={};
-    website.link=relatedWebsiteLinks[i].href;
-    website.title=relatedWebsiteLinks[i].innerText;
-    website.description=relatedWebsiteDescriptions[i].innerHTML;
+    var temp=$(relatedWebsiteList[i]);
+    var x=temp.find('a')[0];
+    website.link=x.href;
+    website.title=x.innerText;
+    var y=temp.find('.s').find('.st')[0];
+    website.description=y.innerHTML;
     relatedWebsites.push(website);
   }
   var websites=[];
+  var websiteList=$(page.find('#rso').find('.rgsep')).last().prev().find('.rc');
   var websiteLinks=page.find('.srg').last().find('.g').find('.r').find('a');
   var websiteImages=page.find('.srg').last().find('._lyb').find('a');
   var websiteDescriptions=page.find('.srg').last().find('.g').find('.s').find('.st');
-  for(var i=0;i<websiteLinks.length;i++){
+  for(var i=0;i<websiteList.length;i++){
     var website={};
-    website.link=websiteLinks[i].href;
-    website.title=websiteLinks[i].innerText;
-    var start=websiteImages[i].href.indexOf("imgurl=")+7;
-    var end=websiteImages[i].href.indexOf("&",start);
-    if(end==-1)
-      website.imageUrl=websiteImages[i].href.slice(start);
-    else
-      website.imageUrl=websiteImages[i].href.slice(start,end);
+    var temp=$(websiteList[i]);
+    a=temp;
+    var x=temp.find('a')[0];
+    website.link=x.href;
+    website.title=x.innerText;
+    var y=temp.find('.s').find('.st')[0];
+    website.description=y.innerHTML;
+    var z=temp.find('._lyb').find('a')[0];
+
+    if(z!=null){
+      var start=z.href.indexOf("imgurl=")+7;
+      var end=z.href.indexOf("&",start);
+      if(end==-1)
+        website.imageUrl=z.href.slice(start);
+      else
+        website.imageUrl=z.href.slice(start,end);
+    }
     website.description=websiteDescriptions[i].innerHTML;
     websites.push(website);
   }
@@ -78,7 +90,7 @@ NooBox.Image.fetchFunctions.google=function(cursor,data){
 NooBox.Image.fetchFunctions.baidu=function(cursor,data){
   data=data.replace(/<img[^>]*>/g,"");
   var page=$(data);
-  var keyword=page.find('.guess-info-word-link').text();
+  var keyword=page.find('.guess-info-word-link').text()||page.find('.guess-info-text-link').text();
   var relatedWebsites=[];
   var relatedWebsiteLinks=page.find('.guess-baike').find('.guess-baike-title').find('a');
   var relatedWebsiteDescriptions=page.find('.guess-baike').find('.guess-baike-text');
@@ -99,17 +111,21 @@ NooBox.Image.fetchFunctions.baidu=function(cursor,data){
     relatedWebsites.push(website);
   }
   var websites=[];
-  var websiteImages=page.find('.source-card').find('.source-card-topic-same-image');
-  var websiteLinks=page.find('.source-card').find('.source-card-topic-title').find('a');
-  var websiteDescriptions=page.find('.source-card').find('.source-card-topic-content');
-  for(var i=0;i<websiteLinks.length;i++){
+  var websiteList=page.find('.source-card-topic');
+  for(var i=0;i<websiteList.length;i++){
     var website={};
-    website.link=websiteLinks[i].href;
-    website.title=websiteLinks[i].innerText;
-    website.description=websiteDescriptions[i].innerHTML;
-    var start=websiteImages[i].style.backgroundImage.indexOf('http');
-    var end=websiteImages[i].style.backgroundImage.indexOf('")');
-    website.imageUrl=websiteImages[i].style.backgroundImage.slice(start,end);
+    var temp=$(websiteList[i]);
+    var x=temp.find('a')[0];
+    website.link=x.href;
+    website.title=x.innerText;
+    var y=temp.find('.source-card-topic-content')[0];
+    website.description=y.innerHTML;
+    var z=temp.find('.source-card-topic-same-image')[0];
+    if(z){
+      var start=z.style.backgroundImage.indexOf('http');
+      var end=z.style.backgroundImage.indexOf('")');
+      website.imageUrl=z.style.backgroundImage.slice(start,end);
+    }
     websites.push(website);
   }
   NooBox.Image.result[cursor].baiduKeyword=keyword;
