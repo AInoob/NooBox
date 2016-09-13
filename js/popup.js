25b6563128d7273c169e6d92227e79dfc505daa4
ainoob <ainoob@qq.com>
2016-09-13 02:14:59 -0500

    update popup options

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,55 @@
 $('.switch').each(function(index,element){
   isOn(element.id,function(){
     element.checked=true;
+    updateSubSection.bind(element)(true);
+  },function(){
+    element.checked=false;
+    updateSubSection.bind(element)(false);
   });
   $(element).change(function(){
     if(this.checked){
-      set(this.id,'1',function(){
-        console.log({job: element.id});
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {job: element.id}, function() {});
-        });
-      });
+      set(element.id,'1',setOn.bind(element));
     }
     else{
-      set(this.id,'-1',function(){
-        console.log({job: element.id});
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {job: element.id}, function() {});
-        });
-      });
+      set(element.id,'-1',setOff.bind(element));
     }
   });
 });
+
+var setOn=function(){
+  updateSubSection.bind(this)(true);
+  chrome.extension.sendMessage({job: this.id}, function() {});
+  chrome.tabs.query({}, function(allTabs) {
+    for(i in allTabs){
+      chrome.tabs.sendMessage(allTabs[i].id, {job: this.id}, function() {});
+    }
+  });
+}
+
+var setOff=function(){
+  updateSubSection.bind(this)(false);
+  chrome.extension.sendMessage({job: this.id}, function() {});
+  chrome.tabs.query({}, function(allTabs) {
+    for(i in allTabs){
+      chrome.tabs.sendMessage(allTabs[i].id, {job: this.id}, function() {});
+    }
+  });
+}
+
+updateSubSection=function(display){
+  var hasSub=false;
+  for(i in this.classList){
+    if(this.classList[i]=='hasSub'){
+      hasSub=true;
+    }
+  }
+  if(hasSub){
+    if(display)
+      $(this).parent().next().show();
+    else
+      $(this).parent().next().hide();
+  }
+}
 
 function isOn(key,callbackTrue,callbackFalse){
   get(key,function(value){
