8fe92ce138f17b68a8e6dcf444d98eebcb866b48
ainoob <ainoob@qq.com>
2016-10-16 22:42:00 -0500

    fix sitemap bug, ready to screenshot search

--- js/noobox.js
+++ js/noobox.js
@@ -1,8 +1,17 @@
 init=function(){
   $('.word').each(function(index,element){
-    console.log(element);
     element.innerHTML=chrome.i18n.getMessage(element.getAttribute('word'));
   });
 }
 
 document.addEventListener( "DOMContentLoaded", init, false );
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-77112662-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
