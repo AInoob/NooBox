2c917d2e7b76868e4c3fa102579c049a8e2f5869
ainoob <ainoob@qq.com>
2016-10-23 21:10:14 -0500

    fix some typos and some black translation

--- js/noobox.js
+++ js/noobox.js
@@ -1,10 +1,20 @@
 init=function(){
   $('.word').each(function(index,element){
-    element.innerHTML=chrome.i18n.getMessage(element.getAttribute('word'));
+    console.log(element.getAttribute('word'));
+    if(element.tagName=="TITLE"){
+      element.innerHTML=capFirst(chrome.i18n.getMessage(element.getAttribute('word')));
+    }
+    else{
+      element.innerHTML=chrome.i18n.getMessage(element.getAttribute('word'));
+    }
   });
 }
 
 document.addEventListener( "DOMContentLoaded", init, false );
+
+var capFirst=function(word){
+  return word[0].toUpperCase()+word.slice(1);
+}
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-77112662-2']);
