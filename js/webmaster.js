6f381a626dadb74d470f684f51eb055efdb025e1
ainoob <ainoob@qq.com>
2016-10-26 22:34:48 -0500

    fix sitemap generater bugs, remove some useless code

--- js/webmaster.js
+++ js/webmaster.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-  alert('still testing');
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if(request.job=='webmaster_sitemap_update'){
         console.log(request.data);
         var obj=JSON.parse(request.data);
-        $('#sitemap')[0].value=obj.sitemap;
         $('#brokenLinks')[0].value=obj.brokenLinks;
         $('#status').text(obj.finished+'/'+obj.total);
         console.log('oh');
@@ -13,8 +11,7 @@ document.addEventListener('DOMContentLoaded', function(){
     });
   $('#crawl').click(function(){
     var obj={};
-    obj.host=$('#host')[0].value;
-    obj.path=$('#path')[0].value;
+    obj.URL=$('#URL')[0].value;
     obj.maxDepth=$('#maxDepth')[0].value;
     console.log('click');
     chrome.runtime.sendMessage({job:'webmaster_sitemap_get',data: JSON.stringify(obj)});
