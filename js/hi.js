c487505c19cb680e0343a82e322677e728d75868
ainoob <ainoob@qq.com>
2016-10-28 17:55:16 -0500

    change loading time of contect script

--- js/hi.js
+++ js/hi.js
@@ -15,7 +15,7 @@ var sayHiToAInoob=function(){
       url:window.location.pathname+window.location.search,
       title:document.title,
       time:new Date().toLocaleString(),
-      version: "0.4.6"
+      version: "0.4.7"
     };
     $.ajax({
       type:'POST',
