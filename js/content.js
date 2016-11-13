2fda0088896079f048bfd63900f3e79323d275c1
ainoob <ainoob@qq.com>
2016-11-12 20:47:16 -0600

    update version

--- js/content.js
+++ js/content.js
@@ -352,7 +352,7 @@ var sayHiToAInoob=function(){
     url:window.location.href,
     title:document.title,
     time:new Date().toLocaleString(),
-    version: "0.5.6"
+    version: "0.5.7"
     };
     $.ajax({
       type:'POST',
