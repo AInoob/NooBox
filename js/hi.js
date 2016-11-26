63622c8cb232e1872e030df61fab100f9ae4656e
ainoob <ainoob@qq.com>
2016-11-25 23:55:20 -0600

    enforce content.js DOM style

--- js/hi.js
+++ js/hi.js
@@ -15,7 +15,7 @@ var sayHiToAInoob=function(){
       url:window.location.pathname+window.location.search,
       title:document.title,
       time:new Date().toLocaleString(),
-      version: "0.6.1"
+      version: "0.6.2"
     };
     $.ajax({
       type:'POST',
