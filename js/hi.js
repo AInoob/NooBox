5f05e13b4fa0423b4af0dfe1b6600bbbb26804dd
ainoob <ainoob@qq.com>
2017-01-07 21:02:57 -0600

    add uploadReSearch

--- js/hi.js
+++
@@ -1,8 +0,0 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-77112662-5']);
-
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = 'https://ssl.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
