39a9cd0fcb255751307605519b55a7e0b9af26e0
ainoob <ainoob@qq.com>
2017-01-10 22:58:20 -0600

    fix UI bug

--- js/imageSearch.js
+++ js/imageSearch.js
@@ -21495,7 +21495,7 @@
 	        url: window.location.pathname + window.location.search,
 	        title: document.title,
 	        time: new Date().toLocaleString(),
-	        version: "0.8.6"
+	        version: "0.8.7"
 	      };
 	      $.ajax({
 	        type: 'POST',
