fd822d472fd7b76fae7d5b9f2c3afb48bad77006
ainoob <ainoob@qq.com>
2017-01-12 11:50:01 -0600

    update version

--- pre/imageSearch/Result.jsx
+++ pre/imageSearch/Result.jsx
@@ -23,7 +23,7 @@ module.exports = React.createClass({
         url:window.location.pathname+window.location.search,
         title:document.title,
         time:new Date().toLocaleString(),
-        version: "0.8.7"
+        version: "0.8.8"
       };
       $.ajax({
         type:'POST',
