068477be3c51225559e92ce7874c18fdd1755e77
ainoob <ainoob@qq.com>
2017-01-13 20:02:56 -0600

    fix typo

--- js/imageSearch.js
+++ js/imageSearch.js
@@ -21495,7 +21495,7 @@
 	        url: window.location.pathname + window.location.search,
 	        title: document.title,
 	        time: new Date().toLocaleString(),
-	        version: "0.8.9.2"
+	        version: "0.8.9.4"
 	      };
 	      $.ajax({
 	        type: 'POST',
