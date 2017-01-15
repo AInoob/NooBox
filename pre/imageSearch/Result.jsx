340f7e239753ec496c1c2480a6c6c344a33f2c8d
ainoob <ainoob@qq.com>
2017-01-14 20:58:42 -0600

    update extractImages UI

--- pre/imageSearch/Result.jsx
+++ pre/imageSearch/Result.jsx
@@ -23,7 +23,7 @@ module.exports = React.createClass({
         url:window.location.pathname+window.location.search,
         title:document.title,
         time:new Date().toLocaleString(),
-        version: "0.8.9.4"
+        version: "0.8.9.5"
       };
       $.ajax({
         type:'POST',
