5ccb1a42b9320506062cf1db11c183d5523b570b
AInoob <noob@ainoob.com>
2017-08-13 10:20:40 -0700

    fix ExtractImages bug!

--- dist/js/extractImages.js
+++ dist/js/extractImages.js
@@ -82,7 +82,7 @@
 	  for (var i = 1; i < val; i++) {
 	    tempFocus2 = $(tempFocus2).parent()[0];
 	  }
-	  getAllImgs = function getAllImgs(elem) {
+	  var getAllImgs = function getAllImgs(elem) {
 	    $(elem).find('*').each(function () {
 	      if (this.tagName == "IMG") {
 	        imgSet[this.src] = true;
