2c0a0b6287a1fe16da0fa5b4fb8af0e994c16a92
RageCoke1466 <ragecoke@ainoob.com>
2018-05-19 09:08:37 -0400

    rountie commit

--- src/imageSearch/Components/SearchContent/SearchTabs.jsx
+++ src/imageSearch/Components/SearchContent/SearchTabs.jsx
@@ -8,6 +8,71 @@ const SearchTabContainer = styled.div`
   margin-top: 30px;
 `;
 export default class SearchTab extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      // activeTab: ,
+      // itemCollector: ,
+      // ithemLength:
+    }
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  componentWillReceiveProps(nextProps){
+    if(nextProps.data){
+      const mapPropsToTab = new Map();
+      this.dealData(nextProps.data,mapPropsToTab);
+      const itemCollector = this.generateTabContent(mapPropsToTab);
+      const numOfTab  = itemCollector.length;
+      const activeTab = numOfTab > 0 ? 0 : 0;
+      //set State of the SearchTab
+      this.setState({
+        activeTab,
+        numOfTab,
+        itemCollector,
+      })
+    }else{
+      console.log("Error");
+    }
+  }
+  handleKeyPress(event){
+    // console.log("keyPressed");
+    // console.log(this.state.activeTab);
+    let activeTab = this.state.activeTab;
+    console.log(event.keyCode);
+    switch(event.keyCode){
+        case 37:
+          if(activeTab - 1 == -1){
+              activeTab = this.state.numOfTab -1;
+          }else{
+            activeTab --;
+          }
+          this.setState({
+            activeTab
+          })
+        break;
+        case 39:
+          if(activeTab + 1 == this.state.numOfTab){
+              activeTab = 0;
+          }else{
+            activeTab ++;
+          }
+          this.setState({
+            activeTab
+          })
+        break;
+        default:
+        //do nothing
+    }
+  }
+
+ componentDidMount(){
+   console.log("here");
+   document.onkeydown = this.handleKeyPress;
+ }
+//  componentWillUnmount(){
+//   window.removeEventListener('keypress', this.handleKeyPress);
+//  }
+
   dealData(data,map){
     data.map((element,index) => {
       if(map.has(element["searchEngine"])){
@@ -24,10 +89,10 @@ export default class SearchTab extends React.Component{
     for(let item of map){
       ItemCollector[ItemCollector.length] = item;
     }
-   
     return ItemCollector;
   }
   render(){
+
     if(!this.props.data){
       return(
         <div>
@@ -37,18 +102,13 @@ export default class SearchTab extends React.Component{
         </div>
       );
     }
-    const mapPropsToTab = new Map();
-    this.dealData(this.props.data,mapPropsToTab);
-    const ItemCollector = this.generateTabContent(mapPropsToTab);
-    //console.log(ItemCollector);
     return (
       <SearchTabContainer>
         <Row>
           <Col span = {2}></Col>
           <Col span = {20}>
-            <Tabs type="card">
-              {ItemCollector.map((element,index) =>{
-                console.log(element);
+            <Tabs type="card" activeKey = {this.state.activeTab+""} >
+              {this.state.itemCollector.map((element,index) =>{
                 return(
                   <TabPane key = {index} tab={element[0]}>
                     <TabContent data ={element[1]}/>
@@ -57,11 +117,8 @@ export default class SearchTab extends React.Component{
               })}
             </Tabs>
           </Col>
-          
         </Row>
-        
       </SearchTabContainer>
-      
     );
   };
 }
