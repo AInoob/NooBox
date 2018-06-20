44c00f621f0116338fa3c758b950e4818b4e87e5
RageCoke1466 <ragecoke@ainoob.com>
2018-06-19 22:48:02 -0400

    add styleComponent

--- src/popupRouter.js
+++ src/popupRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import PopupTest from './routes/popup/PopupTest.jsx';
+import Noobox from './routes/popup/Noobox.jsx';
 import  {OVERVIEW,HISTORY,OPTIONS,ABOUT} from "./common/navURL";
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={PopupTest} />
+        <Route path="/:tab_name" exact component={Noobox} />
       </Switch>
     </Router>
   );
