5eadd211dc7ad53380018df9a58b72b1195b29ee
George_Mac <yang.2766@osu.edu>
2018-06-19 22:48:02 -0400

    add styleComponent

---
+++ src/routes/popup/Noobox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+const Noobox = styled.div`
+  width: 480px;
+`;
+export default class Noobox extends React.Component{
+  render(){
+    return(
+      <div>
+        Test C hange
+        Hello World, This is Popup
+      </div>
+    )
+  }
+}
