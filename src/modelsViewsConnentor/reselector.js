3ca01585add569a920bcf6d152101e6f8adf5fb0
RageCoke1466 <ragecoke@ainoob.com>
2018-06-30 03:55:42 -0400

    finish auto refresh

--- src/modelsViewsConnentor/reselector.js
+++ src/modelsViewsConnentor/reselector.js
@@ -1,7 +1,20 @@
 import {createSelector} from 'reselect';
 
 export const nooboxSelector      = (state) => state.noobox;
-export const overviewSelector    = (state) => state.overview;
+export const overviewSelector    = (state) => {
+  const overview  = state.overview;
+  // console.log(overview);
+  let autoRefresh = {
+    ifRefresh: overview.ifRefresh,
+    refreshInterval:overview.refreshInterval,
+    refreshElapsed:overview.refreshElapsed,
+  }
+  return{
+    inited:overview.inited,
+    tabId:overview.tabId,
+    autoRefresh,
+  }
+};
 export const userHistorySelector = (state) => state.userHistory;
 export const optionsSelector     = (state) => state.options;
 export default createSelector(
