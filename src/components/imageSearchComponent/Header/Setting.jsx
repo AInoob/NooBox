b7975eae6db33ca56fa66c6fde20e9fdd018bbce
RageCoke1466 <ragecoke@ainoob.com>
2018-07-02 00:50:25 -0400

    working on image Search display

--- src/components/imageSearchComponent/Header/Setting.jsx
+++ src/components/imageSearchComponent/Header/Setting.jsx
@@ -4,36 +4,17 @@ import { Card,Tooltip } from 'antd';
 import FAIcon from '@fortawesome/react-fontawesome';
 import faSolid from '@fortawesome/fontawesome-free-solid';
 const { Meta } = Card;
-const BriefContainer = styled.div`
+const SettingContainer = styled.div`
 
 `;
-export default class Engine extends React.Component{
+export default class Setting extends React.Component{
   render(){
     console.log(faSolid)
     let inited = true;
-    if(!inited){
-      //do something
-    }else{
       return(
-        <BriefContainer>
-            <Card
-              style={{ width: 200 }}
-              cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-              actions={[<Tooltip placement = "top" title="Download">
-                         <FAIcon icon ={faSolid.faDownload}/>
-                        </Tooltip>,
-                        <Tooltip placement = "top" title="Search Again">
-                          <FAIcon icon ={faSolid.faUpload}/>
-                        </Tooltip>
-                       ]}
-            >
-              <Meta
-                title="Image Keyword"
-                description="This is the Keyword"
-              />
-            </Card>
-        </BriefContainer>
+        <SettingContainer>
+           Setting
+        </SettingContainer>
       )
-    }
   }
 }
