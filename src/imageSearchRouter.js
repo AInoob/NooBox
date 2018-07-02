59d5bf8c1ba74aa2b98e2119c741400b08856c74
RageCoke1466 <ragecoke@ainoob.com>
2018-07-02 00:59:42 -0400

    merge

--- src/imageSearchRouter.js
+++ src/imageSearchRouter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import ImageSearchTest from './routes/imageSearch/ImageSearchTest.jsx';
+import ImageSearchResult from './routes/imageSearch/ImageSearchResult.jsx';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={ImageSearchTest} />
+        <Route path="/" exact component={ImageSearchResult} />
       </Switch>
     </Router>
   );
