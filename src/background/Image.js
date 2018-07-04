e232c11f2adeca448ded7ff48f7a656c6c330501
AInoob <noob@ainoob.com>
2018-07-04 11:20:02 -0700

    import extract image & options

---
+++ src/background/Image.js
@@ -0,0 +1,53 @@
+import data from './data';
+import { logEvent } from '../utils/bello';
+import GL from '../utils/getLocale';
+import { get } from '../utils/db';
+
+export default class Image {
+  constructor() {}
+  async init() {
+    if (await get('extractImages')) {
+      data.Image.extractImageHandle = browser.contextMenus.create({
+        "id": "extractImages",
+        "title": GL("extract_images"),
+        "contexts": ["all"],
+        "onclick": this.extractImages
+      });
+    }
+    else {
+      browser.contextMenus.remove(data.Image.extractImageHandle);
+      data.Image.extractImageHandle = null;
+    }
+  }
+  extractImages(info, tab) {
+    try {
+      browser.tabs.sendMessage(tab.id, {
+        job: "extractImages"
+      }, {
+          frameId: info.frameId
+        }, (response) => {
+          if (!response) {
+            browser.notifications.create('extractImages', {
+              type: 'basic',
+              iconUrl: '/static/nooboxLogos/icon_128.png',
+              title: GL("extractImages"),
+              message: GL("ls_4")
+            }, () => {});
+          }
+        });
+    } catch (e) {
+      browser.tabs.sendMessage(tab.id, {
+        job: "extractImages"
+      }, (response) => {
+        if (!response) {
+          brwoser.notifications.create('extractImages', {
+            type: 'basic',
+            iconUrl: '/static/nooboxLogos/icon_128.png',
+            title: GL("extractImages"),
+            message: GL("ls_4")
+          }, () => {});
+        }
+      });
+    }
+  }
+}
