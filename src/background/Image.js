99460577aabfbcc623b2fc21989824c0445188aa
George_Mac <yang.2766@osu.edu>
2018-07-04 22:04:42 -0400

    merge

--- src/background/Image.js
+++ src/background/Image.js
@@ -129,10 +129,6 @@ export default class Image {
         //'User-Agent': 'Mozilla/4.0 MDN Example',
         'Content-Type': 'application/json'
       },
-<<<<<<< HEAD
-=======
-      mode:"cors",
->>>>>>> Raynor
       body: JSON.stringify({data:base64}),
     }
     let result = await ajax(aionobServer, requestBody);
