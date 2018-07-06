8923e279a8a2a71dd46296be608206dc120f8996
George_Mac <yang.2766@osu.edu>
2018-07-06 02:30:02 -0400

    Add options

--- src/popup/actions.js
+++ src/popup/actions.js
@@ -1,7 +1,11 @@
 import {createAction} from 'redux-actions';
 
-export const nooboxTest        =  createAction('noobox/test');
-export const overviewInit      =  createAction('overview/init');
+export const nooboxTest         =  createAction('noobox/test');
+export const overviewInit       =  createAction('overview/init');
 //Auto Refresh
-export const autoRefreshUpdate =  createAction('overview/autoRefreshUpdate');
-export const imageSearchBegin  =  createAction('overview/imageSearchBegin');
+export const autoRefreshUpdate  =  createAction('overview/autoRefreshUpdate');
+export const imageSearchBegin   =  createAction('overview/imageSearchBegin');
+export const optionsOnCheck     =  createAction('options/onChecked');
+export const optionsCheckEngine =  createAction('options/onCheckEngine');
+export const optionsCheckTool   =  createAction('options/onCheckTool');
+export const optionsCheckExp    =  createAction('options/onCheckExp');
