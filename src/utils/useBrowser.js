03b2613534a97747b7eb6563dea6bdc10b2154a5
George_Mac <yang.2766@osu.edu>
2018-07-07 21:38:53 -0400

    add language Pack

--- src/utils/useBrowser.js
+++ src/utils/useBrowser.js
@@ -1,3 +1,9 @@
 export default () => {
   window.browser = chrome;
+  window["__"]   = (string) =>{
+    // console.log(string);
+    // console.log(chrome.i18n.getMessage("video_control"));
+
+    return chrome.i18n.getMessage(string)
+  }
 }
