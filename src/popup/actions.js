ec7b52a09ab808273b4c07da965d3cddf01936c0
George_Mac <yang.2766@osu.edu>
2018-07-25 11:51:07 +0800

    working on history

--- src/popup/actions.js
+++ src/popup/actions.js
@@ -6,7 +6,10 @@ export const overviewInit       =  createAction('overview/init');
 export const autoRefreshUpdate  =  createAction('overview/autoRefreshUpdate');
 export const imageSearchBegin   =  createAction('overview/imageSearchBegin');
 
+export const userHistoryInit    =  createAction('userHistory/init');
+
 export const optionsInit        =  createAction('options/init');
 export const optionsCheckEngine =  createAction('options/onCheckEngine');
 export const optionsCheckTool   =  createAction('options/onCheckTool');
 export const optionsCheckExp    =  createAction('options/onCheckExp');
+
