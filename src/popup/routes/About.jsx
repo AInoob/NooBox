c2a52bf292085c9d74c2dd98c98ae0a8d028148e
AInoob <noob@ainoob.com>
2018-08-11 19:14:31 -0700

    update locale & webpack

--- src/popup/routes/About.jsx
+++ src/popup/routes/About.jsx
@@ -24,29 +24,47 @@ const customPanelStyle = {
   padding:0,
   overflow: 'hidden',
 };
-const privacyData =[
+const privacyData = [
   {
     title: i18n("about_privacy_message_0"),
-    description : i18n("about_privacy_message_1"),
-    type :"1"
+    description: i18n("about_privacy_message_1"),
+    type: "1"
   },
   {
     title: i18n("about_privacy_message_2"),
-    description : i18n("about_privacy_message_3"),
-    type :"1"
+    description: i18n("about_privacy_message_3"),
+    type: "1"
   },
   {
     title: i18n("about_privacy_message_4"),
-    description : i18n("about_privacy_message_5"),
-    type :"1"
+    description: i18n("about_privacy_message_5"),
+    type: "1"
   },
   {
     title: i18n("about_privacy_message_6"),
-    description : i18n("about_privacy_message_7"),
-    type :"2"
+    description: i18n("about_privacy_message_7"),
+    type: "2"
   },
 ];
-export default class About extends React.Component{
+
+const contributionData = [{
+  title: i18n('about_contribution_zhtw2013'),
+  description: i18n('about_contribution_zhtw2013_description'),
+  icon: 'https://avatars1.githubusercontent.com/u/9501406?s=100&v=4',
+  link: 'https://github.com/zhtw2013'
+}, {
+  title: i18n('about_contribution_george'),
+  description: i18n('about_contribution_grorge_description'),
+  icon: 'https://avatars0.githubusercontent.com/u/12090689?s=100&v=4',
+  link: 'https://github.com/RageCoke1466'
+}, {
+  title: i18n('about_contribution_ainoob'),
+  description: i18n('about_contribution_ainoob_description'),
+  icon: 'https://ainoob.com/favicon/apple-icon-120x120.png',
+  link: 'https://github.com/AInoob',
+}];
+
+export default class About extends React.Component {
   constructor(props) {
     super(props);
     this.socket = io('https://ainoob.com:443/');
@@ -129,9 +147,21 @@ export default class About extends React.Component{
                 <li>{i18n('autoRefresh')} : {this.state.autoRefresh}</li>
               </ul>
           </Panel>
-          {/* <Panel header = {i18n("about_feedback")} key="4">
-            test4
-          </Panel> */}
+          <Panel header = {i18n("about_contribution")} key="4">
+            <List
+              itemLayout="horizontal"
+              dataSource={contributionData}
+              renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src={item.icon} />}
+                  title={<a href={item.link}>{item.title}</a>}
+                  description={item.description}
+                />
+                </List.Item>
+              )}
+            />
+          </Panel>
         </Collapse>
       </AboutContainer>
     )
