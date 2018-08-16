64d85077f7f45c58bf45aebd196dec7ffdfa944f
RageCoke1466 <ragecoke@ainoob.com>
2018-08-16 11:30:05 -0400

    format code

--- src/popup/actions.js
+++ src/popup/actions.js
@@ -1,23 +1,25 @@
-import {createAction} from 'redux-actions';
+import {
+  createAction
+} from 'redux-actions';
 
-export const nooboxTest         =  createAction('noobox/test');
-export const overviewInit       =  createAction('overview/init');
+export const nooboxTest = createAction('noobox/test');
+export const overviewInit = createAction('overview/init');
 //Auto Refresh
-export const autoRefreshUpdate  =  createAction('overview/autoRefreshUpdate');
-export const imageSearchBegin   =  createAction('overview/imageSearchBegin');
-export const h5WebsiteSwitch    =  createAction('overview/html5VideoWebsiteSwitch');
-
-export const userHistoryInit    =  createAction('userHistory/init');
+export const autoRefreshUpdate = createAction('overview/autoRefreshUpdate');
+export const imageSearchBegin = createAction('overview/imageSearchBegin');
+export const h5WebsiteSwitch = createAction('overview/html5VideoWebsiteSwitch');
+//History
+export const userHistoryInit = createAction('userHistory/init');
 export const userHistoryDeleteSingle = createAction("userHistory/deleteSingle");
 export const userHistoryDeleteAll = createAction("userHistory/deleteAll");
 export const userHistoryLoadHisotry = createAction("userHistory/loadImageHistory");
 
-export const optionsInit        =  createAction('options/init');
-export const optionsCheckEngine =  createAction('options/onCheckEngine');
-export const optionsCheckTool   =  createAction('options/onCheckTool');
-export const optionsCheckExp    =  createAction('options/onCheckExp');
+//options
+export const optionsInit = createAction('options/init');
+export const optionsCheckEngine = createAction('options/onCheckEngine');
+export const optionsCheckTool = createAction('options/onCheckTool');
+export const optionsCheckExp = createAction('options/onCheckExp');
 
-export const aboutInit =  createAction('about/init');
-export const plusOne   =  createAction('about/plusOne');
-
-
+//about
+export const aboutInit = createAction('about/init');
+export const plusOne = createAction('about/plusOne');
