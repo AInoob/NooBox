1ba4327b00dd846eb5ee518265f5d094d3fe0b75
RageCoke1466 <ragecoke@ainoob.com>
2018-08-19 02:28:31 -0400

    add simple relence

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,3 +1,16 @@
 export const BELLO_URL = 'https://ainoob.com/bello/noobox';
 
 export const HISTORY_DB_KEY = 'history_records';
+
+export const ENGINE_DONE = {
+  GOOGLE_DONE :"googleDone",
+  BAIDU_DONE  : "baiduDone",
+  BING_DONE   : "bingDone",
+  TINEYE_DONE : "tineyeDone",
+  YANDEX_DONE : "yandexDone",
+  SAUCENAO_DONE : "saucenaoDone",
+  ASCII2D_DONE : "ascii2dDone",
+  IQDB_DONE : "iqdb",
+}
+export const ENGINE_WEIGHT ={
+}
