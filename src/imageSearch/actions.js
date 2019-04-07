94a725f86e58ce1da68f22b4d7bf572655b5261d
AInoob <noob@ainoob.com>
2019-04-06 23:19:39 -0700

    testing

--- src/imageSearch/actions.js
+++
@@ -1,8 +0,0 @@
-import {createAction} from 'redux-actions';
-
-export const imageSearchInit = createAction("imageSearch/init");
-export const imageSearchUpdateDisplayMode = createAction("imageSearch/updateDisplayMode");
-export const imageSearchSortBy = createAction("imageSearch/updateSortBy");
-export const imageSearchSoryByOrder = createAction("imageSearch/updateSortByOrder");
-export const imageSearchUploadSearch = createAction('imageSearch/uploadSearch');
-export const imageSearchSearchAgain = createAction('imageSearch/searchAgain');
--- src/utils/useBrowser.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-  window.browser = chrome;
-  window["i18n"]   = (string) =>{
-    // console.log(string);
-    // console.log(chrome.i18n.getMessage("video_control"));
-    return chrome.i18n.getMessage(string)
-  }
-}
